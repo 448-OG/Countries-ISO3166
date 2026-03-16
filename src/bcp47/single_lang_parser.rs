8552bf964816cd5c3f5bf4998b1e64c782265f71
448 OG <superuser@448.africa>
2026-03-16 16:26:18 +0300

    Implement PartialEq and Eq
    This allows use in frontend frameworks that need this especially for diffs

--- src/bcp47/single_lang_parser.rs
+++ src/bcp47/single_lang_parser.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 use crate::{BC47LanguageInfo, CountriesIso31661Error, CountriesIso31661Result};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct SingleLanguageTranslationMap {
     pub bcp47_code: String,
     pub translations: HashMap<String, String>,
